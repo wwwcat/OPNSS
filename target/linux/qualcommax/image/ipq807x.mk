eb644531196814e3368b6b6dd332232bab40d2aa
oldcat <wwwcat@126.com>
2025-02-13 22:10:53 +0800

    Update ipq807x.mk
    Signed-off-by: oldcat <wwwcat@126.com>

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -42,6 +42,7 @@ define Device/aliyun_ap8220
 	PAGESIZE := 2048
 	DEVICE_DTS_CONFIG := config@ac02
 	SOC := ipq8071
+	DEVICE_PACKAGES := ipq-wifi-aliyun_ap8220 
 	IMAGE/factory.ubi := append-ubi | qsdk-ipq-factory-nand
 endef
 TARGET_DEVICES += aliyun_ap8220
